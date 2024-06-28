99a81a6c38982c96e16d5857f9c5711caf7ea369
sam-starikov <sam_starikov@rambler.ru>
2024-06-29 01:53:45 +0400

    add responsive

--- script.js
+++ script.js
@@ -2,11 +2,17 @@
 const burgerBtn = document.querySelector('#burger')
 const headerNav = document.querySelector('.header__nav-list')
 const mobileMenu = document.querySelector('#mobile-menu')
+const closeMobileMenuBtn = document.querySelector('.mobile-menu__btn-close')
 
 burgerBtn.addEventListener('click', function () {
     headerNav.classList.toggle('header__nav-list--hidden')
     mobileMenu.classList.toggle('mobile-menu--open')
     this.classList.toggle('header__burger--active')
+})
+closeMobileMenuBtn.addEventListener('click', () => {
+    mobileMenu.classList.remove('mobile-menu--open')
+    burgerBtn.classList.remove('header__burger--active')
+    headerNav.classList.remove('header__nav-list--hidden')
 })
 /* end */
 
@@ -14,10 +20,12 @@ burgerBtn.addEventListener('click', function () {
 const swiper = new Swiper('.swiper', {
     direction: 'horizontal',
     loop: true,
-    // autoplay: true,
+    grabCursor: true,
+    autoplay: true,
 
     navigation: {
         nextEl: '.slider__btn-next',
         prevEl: '.slider__btn-prev',
     },
 })
+/* end */
